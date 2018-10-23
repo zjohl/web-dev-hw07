7087010e0c4a4329d693fa0ff9d2bfb7339416d8
Zamir Johl <zjohl@pivotal.io>
2018-10-23 09:15:44 -0400

    Update prod db username

--- hw07/config/prod.exs
+++ hw07/config/prod.exs
@@ -14,7 +14,7 @@ config :hw07, Hw07Web.Endpoint,
   root: ".",
   version: Application.spec(:phoenix_distillery, :vsn),
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "taks1.zamirjohl.com", port: 80],
+  url: [host: "tasks2.zamirjohl.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
@@ -91,7 +91,7 @@ config :hw07, Hw07Web.Endpoint,
 
 # Configure your database
 config :hw07, Hw07.Repo,
-  username: "hw07",
+  username: "task_manager_2",
   password: get_secret.("db_pass"),
   database: "hw07_prod",
   pool_size: 15
