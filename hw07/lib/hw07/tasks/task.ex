27e92a282125871cd6a478cc589e137fecde8db4
Zamir Johl <zjohl@pivotal.io>
2018-10-23 10:42:08 -0400

    Stop validating time spent

--- hw07/lib/hw07/tasks/task.ex
+++ hw07/lib/hw07/tasks/task.ex
@@ -18,16 +18,5 @@ defmodule Hw07.Tasks.Task do
     task
     |> cast(attrs, [:title, :desc, :completed, :time_spent, :user_id])
     |> validate_required([:title, :desc, :completed, :time_spent, :user_id])
-    |> validate_time_spent
-  end
-
-  def validate_time_spent(changeset) do
-    validate_change(changeset, :time_spent, fn _, time_spent ->
-      cond do
-        (time_spent < 0) -> [{:time_spent,"Can't use negative value"}]
-        (rem(time_spent, 15) != 0) -> [{:time_spent, "Time spent must be multiple of 15"}]
-        true -> []
-      end
-    end)
   end
 end
